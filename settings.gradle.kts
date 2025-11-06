3e3de3ef0299c285bb92975e45a399b9b37b83f9
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2025-11-06 23:09:30 +0700

    benerin tata letak sama main activity

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "belajarlayout_20230140162"
+include(":app")
+ 
